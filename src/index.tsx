f1ef16b4616f6231621a522031aa2d3b517f22c4
Yulia Modestova <jmodestova@yahoo.com>
2019-06-16 18:05:56 +0300

    Fix babel config, add helmet

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './modules/App/App';
+import { App } from './modules/App';
 require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Menu } from './Menu';
+import Helmet from 'react-helmet';
 
 const isDev = process.env.NODE_ENV === 'development';
 
 const App: React.FunctionComponent<{}> = () => (
     <BrowserRouter basename={isDev ? '' : process.env.BASE_URL}>
+        <Helmet>
+            <html lang="en" />
+            <meta charSet="utf-8" />
+            <title>I&apos;m wearing a helmet!</title>
+        </Helmet>
         <Menu />
         <Switch>
             <Route exact path="/" component={() => <div>Main</div>} />
